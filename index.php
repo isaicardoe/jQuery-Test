8944423a760f4ddf7a085b8c8e170b7db7ec95df
isai cardoso <isai.cardoe@gmail.com>
2015-04-20 13:18:34 -0700

    created divs

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <html>
     <head>
+        <title>Dan's Test Bruh</title>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <div class="dan">
+            <div id="iwao">
         <?php
         
         ?>
+            </div>
+        </div>
     </body>
 </html>
